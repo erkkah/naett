469b64fae6f74758e0815e563c19989297181652
Erik Agsjö <erik.agsjo@gmail.com>
2023-05-24 23:37:34 +0200

    Fix issue #9

--- src/naett_linux.c
+++ src/naett_linux.c
@@ -94,7 +94,7 @@ int naettPlatformInitRequest(InternalRequest* req) {
 static size_t readCallback(char* buffer, size_t size, size_t numItems, void* userData) {
     InternalResponse* res = (InternalResponse*)userData;
     InternalRequest* req = res->request;
-    return req->options.bodyReader(buffer, size, req->options.bodyReaderData);
+    return req->options.bodyReader(buffer, size * numItems, req->options.bodyReaderData);
 }
 
 static size_t writeCallback(char* ptr, size_t size, size_t numItems, void* userData) {
